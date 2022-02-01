48f9e8425cec8a0155b9668fa127dd1a290ed6f9
plcs2eco <j_ichiri@yahoo.com>
2022-02-01 18:18:25 +0900

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-// run `node index.js` in the terminal
-
-console.log(`Hello Node.js v${process.versions.node}!`);
+let a = [
+  { name: 'ichiri-age-7', value: '50' },
+  { name: 'ichiri-id-7', value: '80' },
+  { name: 'ichiri-name-8', value: 'ok' },
+  { name: 'ichiri-age-8', value: '60' },
+  { name: 'ichiri-id-8', value: '100' },
+];
+let myJSON = JSON.stringify(a);
+let regex = RegExp('^ichiri-id');
+let arr = a.filter(function (value) {
+  return regex.test(value.name);
+});
+console.log(a);
+console.log(arr);
+console.log(myJSON);
+let myObj = JSON.parse(myJSON);
+console.log(myObj);
