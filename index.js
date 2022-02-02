db11612ac825012d2accb6870c73026eabde4ab9
plcs2eco <j_ichiri@yahoo.com>
2022-02-02 17:42:45 +0900

    1 file modified

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-let a = [
-  { name: 'ichiri-age-7', value: '50' },
-  { name: 'ichiri-id-7', value: '80' },
-  { name: 'ichiri-name-8', value: 'ok' },
-  { name: 'ichiri-age-8', value: '60' },
-  { name: 'ichiri-id-8', value: '100' },
-];
-let myJSON = JSON.stringify(a);
-let regex = RegExp('^ichiri-id');
-let arr = a.filter(function (value) {
-  return regex.test(value.name);
-});
-console.log(a);
-console.log(arr);
-console.log(myJSON);
-let myObj = JSON.parse(myJSON);
-console.log(myObj);
+let a = { name: 'ichiri', gender: 'male' };
+b = JSON.stringify(a);
+c = JSON.stringify(b);
+d = JSON.stringify(c);
+console.log(b);
+console.log(c);
+console.log(d);
